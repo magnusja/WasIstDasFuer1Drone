d5bea8b2d7df3d4159fdb2ea2f3707840fbb521b
Magnus Jahnen <jahnen@in.tum.de>
2016-08-08 12:36:05 +0200

    Some controls

--- main.py
+++ main.py
@@ -9,7 +9,59 @@ logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.DEBUG)
 
 
-def handle_events():
+def handle_controls(drone, event):
+    # takeoff / land
+    if event.key == pygame.K_RETURN:
+        drone.takeoff()
+    elif event.key == pygame.K_SPACE:
+        drone.land()
+    # emergency
+    elif event.key == pygame.K_BACKSPACE:
+        drone.reset()
+    # forward / backward
+    elif event.key == pygame.K_w:
+        drone.move_forward()
+    elif event.key == pygame.K_s:
+        drone.move_backward()
+    # left / right
+    elif event.key == pygame.K_a:
+        drone.move_left()
+    elif event.key == pygame.K_d:
+        drone.move_right()
+    # up / down
+    elif event.key == pygame.K_UP:
+        drone.move_up()
+    elif event.key == pygame.K_DOWN:
+        drone.move_down()
+    # turn left / turn right
+    elif event.key == pygame.K_LEFT:
+        drone.turn_left()
+    elif event.key == pygame.K_RIGHT:
+        drone.turn_right()
+    # speed
+    elif event.key == pygame.K_1:
+        drone.speed = 0.1
+    elif event.key == pygame.K_2:
+        drone.speed = 0.2
+    elif event.key == pygame.K_3:
+        drone.speed = 0.3
+    elif event.key == pygame.K_4:
+        drone.speed = 0.4
+    elif event.key == pygame.K_5:
+        drone.speed = 0.5
+    elif event.key == pygame.K_6:
+        drone.speed = 0.6
+    elif event.key == pygame.K_7:
+        drone.speed = 0.7
+    elif event.key == pygame.K_8:
+        drone.speed = 0.8
+    elif event.key == pygame.K_9:
+        drone.speed = 0.9
+    elif event.key == pygame.K_0:
+        drone.speed = 1.0
+
+
+def handle_events(drone):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             return False
@@ -17,6 +69,8 @@ def handle_events():
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 return False
+
+            handle_controls(drone, event)
 
     return True
 
@@ -45,7 +99,7 @@ def main():
     running = True
 
     while running:
-        running = handle_events()
+        running = handle_events(drone)
         try:
             display(screen, clock, drone)
         except Exception as e:

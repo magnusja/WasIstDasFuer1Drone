d9edf5a43f96143bf9f945fcbff911a198556135
Magnus Jahnen <jahnen@in.tum.de>
2016-08-09 20:54:51 +0200

    Correct python path

--- caffe-classification/run.sh
+++ caffe-classification/run.sh
@@ -1,4 +1,5 @@
 #/bin/bash
 
+export PYTHONPATH=/root/caffe/python/:$PYTHONPATH
 
 python example.py vgg_face_caffe/VGG_FACE.caffemodel vgg_face_caffe/VGG_FACE_deploy.prototxt vgg_face_caffe/ak.png 
